95f0398d8b2cbbe7d44b2efa8bd58bc64f6ac7d9
Hessel <49622357+Heesel@users.noreply.github.com>
2023-10-21 21:21:32 +0200

    nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-      extend: {}
+      extend: {
+        boxShadow: {
+            'menu': 'rgba(136, 165, 191, 0.48) 6px 2px 16px 0px, rgba(255, 255, 255, 0.8) -6px -2px 16px 0px'
+        },
+        colors: {
+            primary: "#507DBC",
+            light: "#808287",
+            dark: "#0C090A"
+        }
+      }
     },
     plugins: []
 };
